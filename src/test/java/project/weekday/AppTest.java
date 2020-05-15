7aa0c831249d85a2b2a229a44c820cef8ad110c9
sangwook.park <ikisis@gmail.com>
2020-05-15 03:57:00 +0000

    first commit

---
+++ src/test/java/project/weekday/AppTest.java
@@ -0,0 +1,23 @@
+package project.weekday;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("test");
+
+        assertEquals(true, true, "true should be true");
+    }
+}
