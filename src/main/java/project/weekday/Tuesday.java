3a795560a7973911676b5d768328c01acacda386
sangwook.park <ikisis@gmail.com>
2020-05-15 08:20:28 +0000

    add spring boot actuator

---
+++ src/main/java/project/weekday/Tuesday.java
@@ -0,0 +1,13 @@
+package project.weekday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuesday {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tuesday.class, args);
+    }
+
+}
--- src/test/java/project/weekday/AppTest.java
+++ src/test/java/project/weekday/AppTest.java
@@ -5,19 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Unit test for simple App.
  */
+@Slf4j
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    @Data
+    @RequiredArgsConstructor
+    public static class Domain {
+        private final int id;
+        private final String name;
+    }
+    
     @Test
     public void shouldAnswerWithTrue()
     {
-        System.out.println("test");
-
+        log.debug("test {}", new Domain(1, "John Doe"));
         assertEquals(true, true, "true should be true");
     }
 }
